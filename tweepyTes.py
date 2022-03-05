242360a6f7c2294a8cc938f1dfdb06eda9c4a888
tech0x86 <kento24n452@yahoo.co.jp>
2022-03-06 00:47:38 +0900

    ツイートを投稿するだけ
    API、アクセスキーのテスト

---
+++ tweepyTes.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# ~/.profile　にアクセス情報を記載
+# 更新を反映するには次のコマンドを実施　$ source ~/.profile
+from tracemalloc import stop
+import tweepy
+import os
+import datetime
+
+CK=os.environ.get("MY_TW_CONSUMER_KEY")
+CS=os.environ.get('MY_TW_CONSUMER_SEC')
+AT=os.environ.get('MY_TW_ACC_KEY')
+AS=os.environ.get('MY_TW_ACC_SEC')
+
+dt_now = datetime.datetime.now()
+
+client = tweepy.Client(consumer_key=CK, consumer_secret=CS, access_token=AT, access_token_secret=AS)
+
+tweetText = "がんばるぞい"+"\n"+ str(dt_now)
+client.create_tweet(text=tweetText)
