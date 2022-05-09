f567158cdbd619d2a5288b42d8ddaaf04f12a74a
tech0x0x86 <kento24n452@yahoo.co.jp>
2022-05-10 00:19:01 +0900

    add tweet with media func

--- tweepyTes.py
+++ tweepyTes.py
@@ -11,9 +11,23 @@ CS=os.environ.get('MY_TW_CONSUMER_SEC')
 AT=os.environ.get('MY_TW_ACC_KEY')
 AS=os.environ.get('MY_TW_ACC_SEC')
 
-dt_now = datetime.datetime.now()
+def tweetTextOnly(twText):
+    dt_now = datetime.datetime.now()
+    client = tweepy.Client(consumer_key=CK, consumer_secret=CS, access_token=AT, access_token_secret=AS)
+    tweetText = twText +"\n"+ str(dt_now)
+    client.create_tweet(text=tweetText)
+    return
 
-client = tweepy.Client(consumer_key=CK, consumer_secret=CS, access_token=AT, access_token_secret=AS)
-# test
-tweetText = "がんばるぞい"+"\n"+ str(dt_now)
-client.create_tweet(text=tweetText)
+def tweetTextMedia(twText, imgPath):
+    auth = tweepy.OAuthHandler(CK, CS)
+    auth.set_access_token(AT, AS)
+    TW_API = tweepy.API(auth)
+    media_ids = []
+    img  = TW_API.media_upload(imgPath)
+    media_ids.append(img.media_id)
+    TW_API.update_status(status=twText, media_ids=media_ids)
+    return
+
+imgPath = "./javanyan.jpg"
+#tweetTextMedia("がんばるぞい",imgPath)
+tweetTextOnly("がんばるぞい")
