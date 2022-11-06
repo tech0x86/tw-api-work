1fce8d8f9b150be2a89be14152e09a566f5a1f59
tech0x0x86 <kento24n452@yahoo.co.jp>
2022-11-06 23:46:41 +0900

    fix

--- tweepyTes.py
+++ tweepyTes.py
@@ -4,10 +4,10 @@
 from tracemalloc import stop
 import tweepy
 import os
-import datetime
+from datetime import datetime, timedelta, timezone
 import random
 imgPath = "./img/"
-
+JST = timezone(timedelta(hours=+9), 'JST')
 
 CK=os.environ.get("MY_TW_CONSUMER_KEY")
 CS=os.environ.get('MY_TW_CONSUMER_SEC')
@@ -29,18 +29,18 @@ def tweetTextMedia(twText, imgName):
     img  = TW_API.media_upload(imgPath + imgName)
     media_ids.append(img.media_id)
     try:
-        result = TW_API.update_status(status=twText, media_ids=media_ids)
-        #print (result)
+        response = TW_API.update_status(status=twText, media_ids=media_ids)
     except Exception as e:
-        print (e)
+        print (e + "ERROR at upload")
+        print(response.text)
     return
 
 if __name__ == '__main__':
     print('start main func')
-    #tweetTextMedia("がんばるぞい",imgPath)
-    #tweetTextOnly("がんばるぞい")
-    #fileリストを取得
+    now = datetime.now(JST).strftime("%Y年%-m月%-d日")
     img_ext = [".jpg",".png"]
+
+    #fileリストを取得
     files = os.listdir(imgPath)
     # ファイルのみリスト形式で取得
     files_file = [f for f in files if os.path.isfile(os.path.join(imgPath, f))]
@@ -49,6 +49,6 @@ if __name__ == '__main__':
     #print (img_files)
     #画像リストの中からランダムにimg nameを一つ選択
     img = img_files[random.randint(0, len(img_files) -1)]
-    
+    print(img + now)
 
-    tweetTextMedia("がんばるぞい　ぞい \n 2022/11/06",img)
+    tweetTextMedia("がんばるぞい \n" + now,img)
